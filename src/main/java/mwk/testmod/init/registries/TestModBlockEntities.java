0f3e1f0c88a42e517b3ad8a8c2f2327c9e9efcfa
MadsWedendahlKruse <madswk2@gmail.com>
2024-02-22 14:40:21 +0100

    Update TestModBlockEntities.java

--- src/main/java/mwk/testmod/init/registries/TestModBlockEntities.java
+++ src/main/java/mwk/testmod/init/registries/TestModBlockEntities.java
@@ -2,7 +2,9 @@ package mwk.testmod.init.registries;
 
 import mwk.testmod.TestMod;
 import mwk.testmod.common.block.multiblock.MultiBlockPartBlockEntity;
+import mwk.testmod.common.block.multiblock.MultiBlockPartBlock;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -16,12 +18,12 @@ public class TestModBlockEntities {
 			DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, TestMod.MODID);
 
 	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<MultiBlockPartBlockEntity>> MULTI_BLOCK_PART_BLOCK_ENTITY_TYPE =
-			BLOCK_ENTITY_TYPES
-					.register("multi_block_part",
-							() -> BlockEntityType.Builder
-									.of(MultiBlockPartBlockEntity::new,
-											TestModBlocks.MACHINE_FRAME_BASIC_BLOCK.get())
-									.build(null));
+			BLOCK_ENTITY_TYPES.register("multi_block_part",
+					() -> BlockEntityType.Builder.of(MultiBlockPartBlockEntity::new,
+							TestModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get)
+									.filter(block -> block instanceof MultiBlockPartBlock)
+									.toArray(Block[]::new))
+							.build(null));
 
 	public static void register(IEventBus modEventBus) {
 		BLOCK_ENTITY_TYPES.register(modEventBus);
