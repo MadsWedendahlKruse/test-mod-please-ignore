9ba87882af43e207419299192a8d3801b31e4c0e
MadsWedendahlKruse <madswk2@gmail.com>
2024-04-24 18:13:54 +0200

    Update CrafterMachineScreen.java

--- src/main/java/mwk/testmod/client/gui/screen/base/CrafterMachineScreen.java
+++ src/main/java/mwk/testmod/client/gui/screen/base/CrafterMachineScreen.java
@@ -1,6 +1,5 @@
 package mwk.testmod.client.gui.screen.base;
 
-import mwk.testmod.TestMod;
 import mwk.testmod.client.gui.widgets.ProgressIcon;
 import mwk.testmod.client.gui.widgets.panels.EnergyPanel;
 import mwk.testmod.client.gui.widgets.panels.PanelManager;
